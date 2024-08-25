66738aceb8a628efe5608b4da80d94cdf8cdca84
Raíssa Dias <raissablw@gmail.com>
2024-08-24 21:30:20 -0300

    composer dotev and database info from env file

--- src/Helper/Connection.php
+++ src/Helper/Connection.php
@@ -3,6 +3,7 @@
 namespace App\Helper;
 
 use mysqli;
+use Dotenv\Dotenv;
 
 class Connection 
 {
@@ -14,6 +15,14 @@ class Connection
 
     public function __construct() 
     {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../'); 
+        $dotenv->load();
+
+        $this->host = $_ENV['HOST'];
+        $this->username = $_ENV['USERNAME'];
+        $this->password = $_ENV['PASSWORD'];
+        $this->db_name = $_ENV['DB_NAME'];
+
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfaa27627a2093a106f5d3950d8c848de
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfaa27627a2093a106f5d3950d8c848de', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfaa27627a2093a106f5d3950d8c848de', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitfaa27627a2093a106f5d3950d8c848de
         call_user_func(\Composer\Autoload\ComposerStaticInitfaa27627a2093a106f5d3950d8c848de::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfaa27627a2093a106f5d3950d8c848de::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
